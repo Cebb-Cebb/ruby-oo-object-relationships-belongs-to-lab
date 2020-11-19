a4e3a3245542fb069da184c8f03492544c3de706
user <github email address>
2020-11-19 15:41:41 -0500

    commit tree faba78a61ead8c8edb0df33952cccce50645444c

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+    attr_accessor :title, :artist
+
+    def initialize
+        @title = title
+        @artist = artist
+    end 
+
+
+
+
+end 
+
+
+# class Song
+
+#     attr_accessor :title, :artist
+  
+#     def initialize(title)
+#       @title = title
+#     end
+#   end 
+
+#   hotline_bling = Song.new("Hotline Bling") 
+#   hotline_bling.title
+#   # => "Hotline Bling" 
+#   hotline_bling.artist = "Drake"
+#   hotline_bling.artist
+#   # => "Drake"
